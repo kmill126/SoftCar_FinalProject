e43a874ebf3fcb31f33b3d3694e4106d2c9449b7
jcole35 <47189370+jcole35@users.noreply.github.com>
2019-04-23 15:52:02 -0400

    Updated with background running via class object pipes

--- weather_widget.py
+++ weather_widget.py
@@ -15,151 +15,159 @@ import tkMessageBox
 import time
 
 import sys
-import multiprocessing
-from multiprocessing import Process, Lock
-import threading
+# import multiprocessing
+from multiprocessing import Process, Pipe
+# import threading
 
 
+class WW:
+    '''
+
+    '''
+    def __init__(self):
+        self.c1, self.c2 = Pipe()
+
+
+    # def manual_check():
+    #     often_value = often.get()
+    #     phone_number = num.get()
+    #     print often_value
+    #     print phone_number
+    #     try:
+    #         return int(often_value)
+    #     except ValueError:
+    #         tkMessageBox.showinfo("Error", "Entry value must be a number")
+
+    # def continuous_check():
+    #     often_value = often.get()
+    #     phone_number = num.get()
+
+    #     p1 = Process(target=recurring_run, args=(often_value, phone_number,))
+    #     p1.start()
+
+    def recurring_run(self, often_value, phone_number):
+        print often_value
+        print phone_number
+        for i in range(10):
+            print("abc")
+            sys.stdout.flush()
+            time.sleep(2)
+            if self.c2.poll() and self.c2.recv() == "KILL":
+                # exit()
+                break
+        try:
+            return int(often_value)
+        except ValueError:
+            tkMessageBox.showinfo("Error", "Entry value must be a number")
+
+    def close_out(self):
+        self.c1.send("KILL")
+        print("Close out worked")
+        exit()
+    
+    '''
+
+    '''
 def weather_widget():
-	'''
-
-	'''
-	# This is just to show how to get the value out of the input box
-	# it often.get() will just need to be incorporated into the final function
-	
-
-	# def grab_often():
-	# 	often_value = often.get()
-	# 	phone_number = num.get()
-	# 	print often_value
-	# 	print phone_number
-	# 	for i in range(15):
-	# 		print "abc"
-	# 		time.sleep(2)
-	# 	try:
-	# 		return int(often_value)
-	# 	except ValueError:
-	# 		tkMessageBox.showinfo("Error", "Entry value must be a number")
-	# def recurring_run():
-	# 	p1 = Process(target=grab_often, args=())
-	# 	p1.start()
-
-	def continuous_check():
-		often_value = often.get()
-		phone_number = num.get()
-		recurring_run(often_value, phone_number)
-
-	def manual_check():
-		often_value = often.get()
-		phone_number = num.get()
-		print often_value
-		print phone_number
-		try:
-			return int(often_value)
-		except ValueError:
-			tkMessageBox.showinfo("Error", "Entry value must be a number")
 
 
+    ww = WW()
 
-	# Creates the overall window
-	master = PanedWindow()
-	master.pack(fill=BOTH, expand=2)
+    def continuous_check():
+        often_value = often.get()
+        phone_number = num.get()
 
-	# Makes the left pane
-	left_pane = PanedWindow(master, orient=VERTICAL, relief=RAISED, height=220, width=200)
-	master.add(left_pane)
-	left = LabelFrame(left_pane, text=" The Umbrella Project")
-	left_pane.add(left)
+        p1 = Process(target=ww.recurring_run, args=(often_value, phone_number,))
+        p1.start()
+    
+    # def close_out(self):
+    #     self.c1.send("KILL")
+    #     print("Close out worked")
 
-	# Adds in the various parts of the left pane
-	var = StringVar()
-	intro_message = ("This window will allow you to run The Umbrella Project:\n"
-		+ "A program that will check the weather every so often and send "
-		+ "you text alerts to tell you when it starts and stops raining")
-	intro = Label(left, textvariable=var, wraplength=190, pady=2)
-	var.set(intro_message)
-	intro.pack()
+    def manual_check():
+        often_value = often.get()
+        phone_number = num.get()
+        print often_value
+        print phone_number
+        try:
+            return int(often_value)
+        except ValueError:
+            tkMessageBox.showinfo("Error", "Entry value must be a number")
 
-	p_num_label = ("What phone number should alerts be sent to?")
-	p_num = Label(left, text=p_num_label, wraplength=190)
-	p_num.pack()
-	num = Entry(left, bd=5)
-	num.pack()
+    # Creates the overall window
+    master = PanedWindow()
+    master.pack(fill=BOTH, expand=2)
 
-	spacer = Label(left, text=" ", wraplength=190)
-	spacer.pack()
+    # Makes the left pane
+    left_pane = PanedWindow(master, orient=VERTICAL, relief=RAISED, height=220, width=200)
+    master.add(left_pane)
+    left = LabelFrame(left_pane, text=" The Umbrella Project")
+    left_pane.add(left)
 
-	exit_button = Button(left, text = "Quit", command = close_out, height=50)
-	exit_button.pack()
+    # Adds in the various parts of the left pane
+    var = StringVar()
+    intro_message = ("This window will allow you to run The Umbrella Project:\n"
+        + "A program that will check the weather every so often and send "
+        + "you text alerts to tell you when it starts and stops raining")
+    intro = Label(left, textvariable=var, wraplength=190, pady=2)
+    var.set(intro_message)
+    intro.pack()
 
-	# Makes the right pane
-	right_pane = PanedWindow(master, orient=VERTICAL, relief=RAISED, height=220, width=200)
-	master.add(right_pane)
+    p_num_label = ("What phone number should alerts be sent to?")
+    p_num = Label(left, text=p_num_label, wraplength=190)
+    p_num.pack()
+    num = Entry(left, bd=5)
+    num.pack()
 
-	# Makes the top part of the right pane
-	top_right_pane = PanedWindow(right_pane, orient=VERTICAL, relief=RAISED, height=110, width=200)
-	right_pane.add(top_right_pane)
-	top_right = LabelFrame(top_right_pane, text="Recurring Weather Checker")
-	top_right_pane.add(top_right)
+    spacer = Label(left, text=" ", wraplength=190)
+    spacer.pack()
 
-	# Adds in the various parts of the top right pane
-	recurance_label = ("How often (in minutes) would you like to check the weather?")
-	recurance = Label(top_right, text=recurance_label, wraplength=190)
-	recurance.pack()
-	often = Entry(top_right, bd=5)
-	often.pack()
-	run_button = Button(top_right, text = "Run", command = continuous_check)
-	run_button.pack()
+    exit_button = Button(left, text = "Quit", command = ww.close_out, height=50)
+    exit_button.pack()
 
-	# Makes the bottom part of the right pane
-	bottom_right_pane = PanedWindow(right_pane, orient=VERTICAL, relief=RAISED, height=110, width=200)
-	right_pane.add(bottom_right_pane)
-	bottom_right = LabelFrame(bottom_right_pane, text="Current Weather Check")
-	bottom_right_pane.add(bottom_right)
+    # Makes the right pane
+    right_pane = PanedWindow(master, orient=VERTICAL, relief=RAISED, height=220, width=200)
+    master.add(right_pane)
 
-	# Adds in the various parts of the bottom right pane
-	instant_label = ("Manually run the weather checker to see if it is currently raining\n")
-	instant = Label(bottom_right, text=instant_label, wraplength=190)
-	instant.pack()
-	check_button = Button(bottom_right, text = "Check now", command = manual_check)
-	check_button.pack()
+    # Makes the top part of the right pane
+    top_right_pane = PanedWindow(right_pane, orient=VERTICAL, relief=RAISED, height=110, width=200)
+    right_pane.add(top_right_pane)
+    top_right = LabelFrame(top_right_pane, text="Recurring Weather Checker")
+    top_right_pane.add(top_right)
 
-	mainloop()
+    # Adds in the various parts of the top right pane
+    recurance_label = ("How often (in minutes) would you like to check the weather?")
+    recurance = Label(top_right, text=recurance_label, wraplength=190)
+    recurance.pack()
+    often = Entry(top_right, bd=5)
+    often.pack()
+    run_button = Button(top_right, text = "Run", command = continuous_check)
+    run_button.pack()
 
-# def grab_often():
-# 	for i in range(3):
-# 		print "abc"
-# 		time.sleep(2)
-# 	# try:
-# 	# 	return int(often_value)
-# 	# except ValueError:
-# 	# 	tkMessageBox.showinfo("Error", "Entry value must be a number")
+    # Makes the bottom part of the right pane
+    bottom_right_pane = PanedWindow(right_pane, orient=VERTICAL, relief=RAISED, height=110, width=200)
+    right_pane.add(bottom_right_pane)
+    bottom_right = LabelFrame(bottom_right_pane, text="Current Weather Check")
+    bottom_right_pane.add(bottom_right)
 
-# def recurring_run(often_value, phone_number):
-# 	p1 = Process(target=grab_often, args=(often_value, phone_number))
-# 	p1.start()
+    # Adds in the various parts of the bottom right pane
+    instant_label = ("Manually run the weather checker to see if it is currently raining\n")
+    instant = Label(bottom_right, text=instant_label, wraplength=190)
+    instant.pack()
+    check_button = Button(bottom_right, text = "Check now", command = manual_check)
+    check_button.pack()
+    
+    mainloop()
 
-# 	def close_out():
-# 		p1.terminate()
-
-def recurring_run(often_value, phone_number):
-		print often_value
-		print phone_number
-		for i in range(15):
-			print "abc"
-			# if close_out():
-			# 	p1.terminate()
-			time.sleep(2)
-		try:
-			return int(often_value)
-		except ValueError:
-			tkMessageBox.showinfo("Error", "Entry value must be a number")
-def close_out():
-# 	if booli == True:
-		exit()
-# 	return booli
 
 if __name__ == "__main__":
-	weather_widget()
-	# recurring_run()
+    weather_widget()
+    # c1, c2 = Pipe()
+    # p1=Process(target=recurring_run, args=(15, 16, c2))
+    # p1.start()
+    # time.sleep(5)
+    # close_out()
+
+
+
 
