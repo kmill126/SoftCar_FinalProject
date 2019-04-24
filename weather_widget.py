fb18569fdb03ddf0d4983485825880e24af9f4fc
jcole35 <47189370+jcole35@users.noreply.github.com>
2019-04-23 22:48:16 -0400

    added in the texting capabilities and more
    more includes checking current condition, comparing to previous condition, and then outputting accordingly as well as attaching required functionality to other parts of the widget.

--- weather_widget.py
+++ weather_widget.py
@@ -1,105 +1,244 @@
-# Might need to check that you can press button and run loop and while loop is running
-# you can press a different button to do something else (here exit) might need to 
-# do parallel stuff maybe
+'''
+The Umbrella Project, final project for Software Carpentry
+Authors: Josh Cole and Katherine Miller
+Date & Time Due: 4/29/19 11:59 pm
 
-# Need to make sure weather runs in the background so you can access other widget pieces
-# if name = main
-# arg1 =
-# arg2 = 
-# etc
-# p1 = Process(target= function_name, args=( , , ))
-# p1.start()  this will run the thing in the background and continue the code onwards
+More to be written
+
+**Functions**
+    Needs to be written
+
+**Classes**
+    WW
+        *Functions*
+            recurring_run
+                Checks weather/sends messages in defined time frame
+            close_out
+                Stops the recurring run
+'''
 
 from tkinter import *
-import tkMessageBox
+from tkinter import messagebox
 import time
-
+import smtplib
 import sys
-# import multiprocessing
 from multiprocessing import Process, Pipe
-# import threading
-
+from bs4 import BeautifulSoup
+import requests
+import datetime
 
 class WW:
     '''
-
+    This is an object that holds pipes for two functions to
+    communicate, and the functions that run and stop a continuous
+    weather check.
     '''
     def __init__(self):
+        '''
+        This initializes the process pipes that allow the 
+        functions to communicate
+        '''
         self.c1, self.c2 = Pipe()
 
-
-    # def manual_check():
-    #     often_value = often.get()
-    #     phone_number = num.get()
-    #     print often_value
-    #     print phone_number
-    #     try:
-    #         return int(often_value)
-    #     except ValueError:
-    #         tkMessageBox.showinfo("Error", "Entry value must be a number")
-
-    # def continuous_check():
-    #     often_value = often.get()
-    #     phone_number = num.get()
-
-    #     p1 = Process(target=recurring_run, args=(often_value, phone_number,))
-    #     p1.start()
-
     def recurring_run(self, often_value, phone_number):
-        print often_value
-        print phone_number
-        for i in range(10):
-            print("abc")
-            sys.stdout.flush()
-            time.sleep(2)
+        '''
+        Checks the weather in a continuous loop that runs
+        on a defined time interval and is stopped by the
+        close_out function. Is triggered as a process by
+        the continuous_check function in the weather_widget
+
+        **Parameters**
+            self: *object*
+                This WW object instance
+            often_value: *int*
+                The number of minutes for how often the function
+                will check the weather
+            phone_number: *str*
+                The 10 digit phone number with attached carrier @ tag
+                to send the alert to. Ex. 1234567890@mms.att.net
+        '''
+
+        # Starts checking the weather at a given time interval
+        i = 0
+        mode = "Umbrella"
+        while True:
+            if i % (often_value * 60) == 0:
+                weather = grab_baltimore_weather()
+                
+                # If some form of rain is happening, and previously wasn't then alert
+                if "Rain" in weather or "Shower" in weather \
+                        or "Storm" in weather or "Snow" in weather \
+                        or "Sleet" in weather or "storm" in weather:
+                    if mode == "Clear":
+                        alert = ("Precipitation currently occuring\n"
+                            + "Umbrella highly advised")
+                        mode = "Umbrella"
+
+                        #Sends a text message containing the above alert
+                        server = smtplib.SMTP( "smtp.gmail.com", 587 )
+                        server.starttls()
+                        server.login( 'SoftwareCarpentry1@gmail.com', 'SoftCar1')
+                        server.sendmail( 'SoftwareCarpentry1', phone_number, alert)
+
+                # If not some form of rain, and previously was then alert
+                else:
+                    if mode == "Umbrella":
+                        alert = ("Weather has cleared\n"
+                            + "Umbrella no longer necessary")
+                        mode = "Clear"
+
+                        #Sends a text message containing the above alert
+                        server = smtplib.SMTP( "smtp.gmail.com", 587 )
+                        server.starttls()
+                        server.login( 'SoftwareCarpentry1@gmail.com', 'SoftCar1')
+                        server.sendmail( 'SoftwareCarpentry1', phone_number, alert)
+
+            # Checks if the weather checking loop should be stopped
             if self.c2.poll() and self.c2.recv() == "KILL":
-                # exit()
+                messagebox.showinfo("Recurring Weather Checker", 
+                    "Your phone is successfully unpaired")
                 break
-        try:
-            return int(often_value)
-        except ValueError:
-            tkMessageBox.showinfo("Error", "Entry value must be a number")
+
+            time.sleep(2)
+            i += 2
 
     def close_out(self):
+        '''
+        Stops the continuous loop that is running from the
+        recurring_run function and creates a confirmation pop up.
+        Is triggered by the exit_button in the weather_widget.
+
+        **Parameters**
+            self: *object*
+                This WW object instance
+
+        **Returns**
+            None
+        '''
         self.c1.send("KILL")
-        print("Close out worked")
-        exit()
-    
+        # exit()
+
+def grab_baltimore_weather():
+    '''
+    Searches the website "https://weather.com/weather/today/l/21218:4:US"
+    for the current weather conditions and returns that
+
+    **Parameters**
+        None
+
+    **Returns**
+        weather: *str*
+            The current weather acording to the website contained in > <
+            Example: >Light Rain<
     '''
 
-    '''
+    webpage = "https://weather.com/weather/today/l/21218:4:US"
+    data = requests.get(webpage)
+    soup = str(BeautifulSoup(data.text, features='lxml'))
+    looking_for = 'class="today_nowcard-phrase"'
+    chopped_text = soup.split(looking_for)
+    start_is_weather = chopped_text[1].split("/")
+    weather = start_is_weather[0]
+
+    return weather
+
 def weather_widget():
+    '''
+    Starts a user interface window that can be used to check the
+    current weather, or have a text sent to you when it starts
+    or stops raining
 
+    **Parameters**
+        None, but some things are user input in the user interface
+
+    **Returns**
+        None, but will create pop up messages and send text messages
+    '''
 
     ww = WW()
 
     def continuous_check():
-        often_value = often.get()
-        phone_number = num.get()
+        '''
+        Makes sure that user inputs to the user interface are in the
+        correct form, and if so starts the recurring_run as a process
+        in the background. If they are incorrect, then creates pop up
+        error messages telling user how inputs should be entered
+        
+        **Parameters**
+            None, but some things are user input in the interface
 
-        p1 = Process(target=ww.recurring_run, args=(often_value, phone_number,))
-        p1.start()
-    
-    # def close_out(self):
-    #     self.c1.send("KILL")
-    #     print("Close out worked")
+        **Returns**
+            None, but will create pop up messages
+        '''
 
-    def manual_check():
-        often_value = often.get()
-        phone_number = num.get()
-        print often_value
-        print phone_number
+        check1 = False
+        check2 = False
+        
+        #Check for correct input of how often to run
         try:
-            return int(often_value)
+            often_value = int(often.get())
+            check1 = True
+
+            #Checks for correct input of phone number and carrier
+            try:
+                phone_number = str(num.get()) + str(carrier.get())
+                server = smtplib.SMTP( "smtp.gmail.com", 587 )
+                server.starttls()
+                server.login( 'SoftwareCarpentry1@gmail.com', 'SoftCar1')
+                test_msg = "Your phone is successfully paired!"
+                server.sendmail( 'SoftwareCarpentry1', phone_number, test_msg)
+
+                answer = messagebox.askyesno("Phone Pairing", 
+                    "Did you recieve a pairing message?\n\n"
+                    + "Please note, this may take a few seconds.")
+                if answer == True:
+                    check2 = True
+                else:
+                    messagebox.showinfo("Error", 
+                        "Please check that your phone number is 10 digits "
+                        + "(no dashes) and the correct carrier "
+                        + "has been selected and try again.")
+            except:
+                messagebox.showinfo("Error", 
+                    "Please check that your phone number is 10 digits "
+                    + "(no dashes) and the correct carrier "
+                    + "has been selected and try again.")
+            
         except ValueError:
-            tkMessageBox.showinfo("Error", "Entry value must be a number")
+            messagebox.showinfo("Error", "Entry value must be a number")
+
+        # If both inputs are correct and user indicates phone has been 
+        # successfully connected to, then starts the process of the
+        # recurring_run function
+        if check1 and check2:
+            p1 = Process(target=ww.recurring_run, args=(often_value, phone_number))
+            p1.start()
+    
+    def manual_check():
+        '''
+        Checks the current weather and creates a pop up message stating it
+
+        **Parameters**
+            None
+
+        **Returns**
+            None, but will create a pop up message
+        '''
+        weather = grab_baltimore_weather()
+        messagebox.showinfo("Current Weather", "The weather outside is:\n\n"
+            + weather)
+
+    
+    ##############################################################################  
+    #                 Creation of the User Interface Window                      #
+    ##############################################################################
 
     # Creates the overall window
     master = PanedWindow()
     master.pack(fill=BOTH, expand=2)
 
     # Makes the left pane
-    left_pane = PanedWindow(master, orient=VERTICAL, relief=RAISED, height=220, width=200)
+    left_pane = PanedWindow(master, orient=VERTICAL, relief=RAISED, height=230, width=200)
     master.add(left_pane)
     left = LabelFrame(left_pane, text=" The Umbrella Project")
     left_pane.add(left)
@@ -107,7 +246,7 @@ def weather_widget():
     # Adds in the various parts of the left pane
     var = StringVar()
     intro_message = ("This window will allow you to run The Umbrella Project:\n"
-        + "A program that will check the weather every so often and send "
+        + "A program that will check Baltimore's weather every so often and send "
         + "you text alerts to tell you when it starts and stops raining")
     intro = Label(left, textvariable=var, wraplength=190, pady=2)
     var.set(intro_message)
@@ -119,18 +258,23 @@ def weather_widget():
     num = Entry(left, bd=5)
     num.pack()
 
-    spacer = Label(left, text=" ", wraplength=190)
+    spacer = Label(left, text="Select carrier: ", width=200, anchor=W)
     spacer.pack()
 
-    exit_button = Button(left, text = "Quit", command = ww.close_out, height=50)
-    exit_button.pack()
+    carrier = StringVar()
+    carrier1 = Radiobutton(left, text = "AT&T", variable=carrier, value="@mms.att.net")
+    carrier1.pack(side=LEFT)
+    carrier2 = Radiobutton(left, text = "T-Mobile", variable=carrier, value="@tmomail.net")
+    carrier2.pack(side=LEFT)
+    carrier3 = Radiobutton(left, text = "Verizon", variable=carrier, value="@vtext.com")
+    carrier3.pack(side=RIGHT)
 
     # Makes the right pane
-    right_pane = PanedWindow(master, orient=VERTICAL, relief=RAISED, height=220, width=200)
+    right_pane = PanedWindow(master, orient=VERTICAL, relief=RAISED, height=230, width=200)
     master.add(right_pane)
 
     # Makes the top part of the right pane
-    top_right_pane = PanedWindow(right_pane, orient=VERTICAL, relief=RAISED, height=110, width=200)
+    top_right_pane = PanedWindow(right_pane, orient=VERTICAL, relief=RAISED, height=160, width=200)
     right_pane.add(top_right_pane)
     top_right = LabelFrame(top_right_pane, text="Recurring Weather Checker")
     top_right_pane.add(top_right)
@@ -141,17 +285,23 @@ def weather_widget():
     recurance.pack()
     often = Entry(top_right, bd=5)
     often.pack()
-    run_button = Button(top_right, text = "Run", command = continuous_check)
-    run_button.pack()
+    warning_message = ("*If you do not press stop before closing, "
+        + "The Umbrella Project will continue running in the background")
+    warning = Label(top_right, text=warning_message, width=200, wraplength=205)
+    warning.pack(side=BOTTOM)
+    run_button = Button(top_right, text = "Run", command = continuous_check, width=12)
+    run_button.pack(side=LEFT)
+    exit_button = Button(top_right, text = "Stop", command = ww.close_out, width = 12)
+    exit_button.pack(side=RIGHT)
 
     # Makes the bottom part of the right pane
-    bottom_right_pane = PanedWindow(right_pane, orient=VERTICAL, relief=RAISED, height=110, width=200)
+    bottom_right_pane = PanedWindow(right_pane, orient=VERTICAL, relief=RAISED, height=70, width=200)
     right_pane.add(bottom_right_pane)
     bottom_right = LabelFrame(bottom_right_pane, text="Current Weather Check")
     bottom_right_pane.add(bottom_right)
 
     # Adds in the various parts of the bottom right pane
-    instant_label = ("Manually run the weather checker to see if it is currently raining\n")
+    instant_label = ("Manually run the weather checker")
     instant = Label(bottom_right, text=instant_label, wraplength=190)
     instant.pack()
     check_button = Button(bottom_right, text = "Check now", command = manual_check)
@@ -162,12 +312,3 @@ def weather_widget():
 
 if __name__ == "__main__":
     weather_widget()
-    # c1, c2 = Pipe()
-    # p1=Process(target=recurring_run, args=(15, 16, c2))
-    # p1.start()
-    # time.sleep(5)
-    # close_out()
-
-
-
-
